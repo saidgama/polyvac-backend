0f6f875368a8b59183555cfca44f6abcf8ea1797
Said Gama <saidgama08@gmail.com>
2025-07-14 13:20:24 -0600

    ok

--- config/cors.js
+++ config/cors.js
@@ -3,7 +3,11 @@ module.exports = {
   origin: process.env.CORS_ORIGINS 
     ? process.env.CORS_ORIGINS.split(',')
     : process.env.NODE_ENV === 'production' 
-      ? ['https://industrias-polyvac.pages.dev']
+      ? [
+          'https://industrias-polyvac.pages.dev',
+          'https://*.pages.dev', // Para builds de preview de Cloudflare
+          'https://cloudflare.com', // Para el proceso de build
+        ]
       : ['http://localhost:4321'],
   
   // Métodos permitidos
